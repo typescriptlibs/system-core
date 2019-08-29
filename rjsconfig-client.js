52fcbb2b21afceaa7251479c7f532ea63b15aced
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2019-08-29 23:10:09 +0200

    Create rjsconfig-client.js

---
+++ rjsconfig-client.js
@@ -0,0 +1,10 @@
+( {
+    generateSourceMaps: true,
+    name: "@tsl/system-core",
+    out: "client/tsl-system-core.js",
+    packages: [{
+        name: "@tsl/system-core",
+        main: "index",
+        location: "client"
+    }]
+} )
