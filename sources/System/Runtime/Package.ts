1109b3f79bb742661dbddb0901ccec3393832aa6
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2019-08-29 23:22:30 +0200

    Create Package.ts

---
+++ sources/System/Runtime/Package.ts
@@ -0,0 +1,100 @@
+/*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*\
+
+    System Core Library
+
+    Copyright (c) TypeScriptLibs and Contributors
+
+    Licensed under the MIT License; you may not use this file except in
+    compliance with the License. You may obtain a copy of the MIT License at
+    https://typescriptlibs.org/LICENSE.txt
+
+\*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*/
+
+////
+//
+//  Imports
+//
+////
+
+import Namespace from './Namespace';
+
+////
+//
+//  Interfaces
+//
+////
+
+export interface PackageClassConstructor extends Function
+{
+    /**
+     * Returns the package of the class. Class packkages are read-only and can
+     * not be changed.
+     */
+    readonly '[[__package__]]'?: string;
+}
+
+export interface PackageClassDecorator extends ClassDecorator
+{
+    $: typeof Namespace;
+}
+
+////
+//
+//  Functions
+//
+////
+
+/**
+ * Decorates the constructor of the following class with a package and optional
+ * namespace for further reflection.
+ *
+ * @example
+ * ```ts
+ *     @Package('@tsl', 'system-core').$('System').$('Runtime')
+ *     export class Environment
+ *     {
+ *         // => package: '@tsl/system-core'
+ *         // => namespace: 'System.Runtime'
+ *     }
+ * ```
+ *
+ * @param packages
+ * One or more packages of the class. All packages will be separated with a
+ * slash (`/`) character.
+ */
+export function Package ( ...packages: Array<string> ): PackageClassDecorator
+{
+    const decorator = function <T extends PackageClassConstructor> ( target: T ): T
+    {
+        if ( target['[[__package__]]'] )
+        {
+            throw new Error( 'Target has already a package!' );
+        }
+
+        Object.defineProperty( target, '[[__package__]]', {
+            configurable: false,
+            enumerable: false,
+            writable: false,
+            value: packages.join( '/' )
+        } );
+
+        return target;
+    } as unknown as PackageClassDecorator;
+
+    Object.defineProperty( decorator, '$', {
+        configurable: false,
+        enumerable: true,
+        writable: false,
+        value: Namespace
+    } );
+
+    return decorator;
+}
+
+////
+//
+//  Export
+//
+////
+
+export default Package;
